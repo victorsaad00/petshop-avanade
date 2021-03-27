37589f3079be1dddeb62a76b9f8f31174e67504c
Victor Saad <v.cavalcanti.saad@avanade.com>
2021-03-27 15:08:54 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const nomePetshop = "PETSHOP AVANADE"
 let moment = require('moment');
+const fs = require('fs');
 
-let pets = require('./db-pets.json');
+const db = require('./db-pets.json');
+let pets = db;
+
+const atualizarBancoDeDados = () => {
+    json = JSON.stringify(pets, null, 2);
+    fs.writeFile("db-pets.json", json, (err)=>{
+        if (err) throw err;
+    });
+}
+
 
 const listarPets = (pets) => {
     for (let pet of pets)
-        console.log(`${pet.nome} ${pet.idade} ${pet.tipo} ${pet.raca}`)
+        console.log(`${pet.nome} ${pet.idade} ${pet.tipo} ${pet.raca}`);
 }
 
 let vacinarPet = (pet) => {
     if (!pet.vacinado) {
         pet.vacinado = true;
         console.log(`${pet.nome} foi vacinado!`);
-        return true
+        return true;
+        
     } else {
         console.log(`${pet.nome} ja é vacinado!`);
-        return false
+        return false;
     }
 }
 
@@ -28,21 +39,15 @@ function campanhaVacina(pets) {
     console.log(`${petsVacinados} vacinados!`);
 }
 
-function adicionarCliente(nome,tipo,idade,raca,peso,tutor,contato,vacinados) {
-    newPet = {
-        nome: nome,
-        tipo: tipo,
-        idade: idade,
-        raca: raca,
-        peso: peso,
-        tutor:tutor,
-        contato: contato,
-        vacinado: vacinados,
-        servicos: []
+function adicionarPet(pet) {
+    if (typeof pet == 'object'){
+        if(!pet.servicos) pet.servicos = [];
+        pets.push(pet);
+        atualizarBancoDeDados();
+        console.log('Pet adicionado com sucesso.');
+    } else {
+        console.log('Não foi possivel adicionar o pet');
     }
-
-    pets.push(newPet);
-
 }
 
 const darBanhoPet = (pet) => {
@@ -60,6 +65,13 @@ const apararUnhasPet = (pet) => {
     console.log(`${pet.nome} está de unhas aparadas!`);
 }
 
-apararUnhasPet(pets[0])
-tosarPet(pets[0])
-darBanhoPet(pets[0])
+// apararUnhasPet(pets[0])
+// tosarPet(pets[0])
+// darBanhoPet(pets[0])
+
+listarPets(pets)
+
+let dogo = 
+{ "nome": "Nicko", "tipo": "Pug", "idade": "1", "raca": "Pug", "peso": 1.8, "tutor": "Odin", "contato": "(81)998677622", "vacinado": false}
+adicionarPet(dogo);
+listarPets(pets)
