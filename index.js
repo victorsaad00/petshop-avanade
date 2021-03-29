df0c21674ed01abf7e62837d89f0f7cab9acd5f9
Victor Saad <v.cavalcanti.saad@avanade.com>
2021-03-29 10:41:07 -0300

    Ajeitado db e algumas outras coisas

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
 const nomePetshop = "PETSHOP AVANADE"
-let moment = require('moment');
-const fs = require('fs');
+let moment = require('moment')
+const fs = require('fs')
 
-const db = require('./db-pets.json');
-let pets = db;
+let db = fs.readFileSync('./db-pets.json')
+db = JSON.parse(db)
+let pets = db.pets
 
 const atualizarBancoDeDados = () => {
-    json = JSON.stringify(pets, null, 2);
-    fs.writeFile("db-pets.json", json, (err)=>{
-        if (err) throw err;
-    });
+    let petsAtualizado = JSON.stringify(db)
+    fs.writeFileSync('db-pets.json', petsAtualizado, 'utf-8')
 }
-
 
 const listarPets = (pets) => {
     for (let pet of pets)
-        console.log(`${pet.nome} ${pet.idade} ${pet.tipo} ${pet.raca}`);
+        console.log(`${pet.nome} ${pet.idade} ${pet.tipo} ${pet.raca}`)
 }
 
 let vacinarPet = (pet) => {
     if (!pet.vacinado) {
-        pet.vacinado = true;
-        console.log(`${pet.nome} foi vacinado!`);
-        return true;
+        pet.vacinado = true
+        console.log(`${pet.nome} foi vacinado!`)
+        return true
         
     } else {
-        console.log(`${pet.nome} ja é vacinado!`);
-        return false;
+        console.log(`${pet.nome} ja é vacinado!`)
+        return false
     }
 }
 
 function campanhaVacina(pets) {
-    let petsVacinados = 0;
+    let petsVacinados = 0
     for(pet of pets) {
         if(vacinarPet(pet))
-            petsVacinados++;
+            petsVacinados++
     }
-    console.log(`${petsVacinados} vacinados!`);
+    atualizarBancoDeDados()
+    console.log(`${petsVacinados} vacinados!`)
 }
 
 function adicionarPet(pet) {
     if (typeof pet == 'object'){
-        if(!pet.servicos) pet.servicos = [];
-        pets.push(pet);
-        atualizarBancoDeDados();
-        console.log('Pet adicionado com sucesso.');
+        if(!pet.servicos) pet.servicos = []
+        pets.push(pet)
+        atualizarBancoDeDados()
+        console.log('Pet adicionado com sucesso.')
     } else {
-        console.log('Não foi possivel adicionar o pet');
+        console.log('Não foi possivel adicionar o pet')
     }
 }
 
 const darBanhoPet = (pet) => {
-    pet.servicos.push('Banho' + moment().format());
-    console.log(`${pet.nome} está de banho tomado!`);
+    pet.servicos.push('Banho' + moment().format())
+    console.log(`${pet.nome} está de banho tomado!`)
 }
 
 const tosarPet = (pet) => {
-    pet.servicos.push('Tosa' + moment().format());
-    console.log(`${pet.nome} está com cabelinho na régua`);
+    pet.servicos.push('Tosa' + moment().format())
+    console.log(`${pet.nome} está com cabelinho na régua`)
 }
 
 const apararUnhasPet = (pet) => { 
-    pet.servicos.push('corte de unhas' + moment().format());
-    console.log(`${pet.nome} está de unhas aparadas!`);
+    pet.servicos.push('corte de unhas' + moment().format())
+    console.log(`${pet.nome} está de unhas aparadas!`)
 }
 
 // apararUnhasPet(pets[0])
@@ -73,5 +72,5 @@ listarPets(pets)
 
 let dogo = 
 { "nome": "Nicko", "tipo": "Pug", "idade": "1", "raca": "Pug", "peso": 1.8, "tutor": "Odin", "contato": "(81)998677622", "vacinado": false}
-adicionarPet(dogo);
+adicionarPet(dogo)
 listarPets(pets)
